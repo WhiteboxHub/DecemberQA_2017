b65eebecee4696168a252155962bf5cad6ae6193
WBL-Training <training@whitebox-learning.com>
2018-02-07 11:02:23 -0800

    Create StringBufferExample.java

---
+++ Java/StringBufferExample.java
@@ -0,0 +1,14 @@
+package com.wbl;
+
+public class StringBufferExample {
+ public static void main(String[] st){
+  StringBuilder sb=new StringBuilder("hello");
+  
+  sb.append("hi");
+  System.out.println(sb);
+  
+  /*StringBuffer sb1=new StringBuffer("HELLO java");
+  sb1.append("hi");
+  System.out.println(sb1);*/
+}
+}
