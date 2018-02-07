24f16f2f3b72c1ca6b0dd7e5baa7088d30590d51
WBL-Training <training@whitebox-learning.com>
2018-02-07 11:03:21 -0800

    Create StringExample.java

---
+++ Java/StringExample.java
@@ -0,0 +1,96 @@
+package com.wbl;
+
+public class StringExample {
+ 
+public static void main(String[] st){
+ String s1="     HELLO java     ";
+ System.out.println(s1+"hi");
+ System.out.println(s1.trim()+"hi");
+ 
+ 
+// s1=s1.toLowerCase();
+// s1=s1.toUpperCase();
+// System.out.println(s1);
+ 
+// int l=s1.length();
+// char[] c=s1.toCharArray();
+// for(int i=0;i<l;i++){
+//  System.out.println(c[i]);
+// }
+ 
+ 
+ 
+ 
+ 
+ 
+ //System.out.println(s1.startsWith("that"));
+ 
+ 
+ 
+String s2="December"+" Batch";
+// String[] w=s1.split("\\s");// splits based on the white space
+// for(String w1:w){
+//  System.out.println(w1);
+//  
+// }
+ 
+ 
+ 
+ 
+// String s4=s1.replace("i","e");
+// System.out.println(s4);
+ 
+ //System.out.println(s1.length());
+   
+   
+// int ind=s1.indexOf("is",6);
+// System.out.println(ind);
+ 
+ 
+ 
+ 
+ 
+ 
+// char c=s1.charAt(4);
+// 
+// System.out.println(c);
+// 
+ 
+ 
+ 
+ 
+ 
+ 
+// System.out.println(s1.substring(0,2));//(statrinx,endinx)
+// System.out.println(s1.substring(3));
+ 
+ 
+ 
+ //s2.concat(" 2017");
+ 
+ //s1=s1.concat("batch");// want decemberbatch...immutable 
+ 
+ String s3=new String("December");
+ 
+// System.out.println(" example equals........");
+// System.out.println(s1.equals(s2));//t
+// System.out.println(s1.equals(s3));//t
+// System.out.println(s2.equals(s3));//t
+// 
+// System.out.println(" example ==........");
+// System.out.println(s1==s2);//t
+// System.out.println(s1==s3);//f
+// System.out.println(s2==s3);//f
+ 
+ 
+ 
+ 
+ System.out.println(s1);
+ 
+ System.out.println(s2);
+ System.out.println(s3);
+ 
+ 
+
+}
+}
